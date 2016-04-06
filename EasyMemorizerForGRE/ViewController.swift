6f94d596fed876bcbbc7eef50d4d1ac6d3e81738
MoreNiceJay <morenicejay@gmail.com>
2016-04-06 13:15:52 +0900

    Initial Commit

---
+++ EasyMemorizerForGRE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyMemorizerForGRE
+//
+//  Created by Lee Janghyup on 4/6/16.
+//  Copyright © 2016 Lee Janghyup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
